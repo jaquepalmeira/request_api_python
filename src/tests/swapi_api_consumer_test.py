db32fdbaab8474d868cd81b2eeca76c291c174a2
Jaqueline Palmeira <jaquepalmeira@gmail.com>
2022-01-20 23:05:59 -0300

    test: Testando o request_mock

--- src/tests/swapi_api_consumer_test.py
+++ src/tests/swapi_api_consumer_test.py
@@ -1,6 +1,8 @@
 from src.infra.swapi_api_consumer import SwapiApiConsumer
 
-def test_get_starships():
+def test_get_starships(requests_mock):
+    
+    requests_mock.get('https://swapi.dev/api/starships/', status_code = 200, json={ 'some': 'thing' })
     swapi_api_consumer = SwapiApiConsumer()
     response = swapi_api_consumer.get_starships(page=1)
     
